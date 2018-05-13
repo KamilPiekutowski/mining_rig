7d9cff4ba40a78e120e5dac31763619a4b9d8df8
Kamil Piekutowski <influence1982@gmail.com>
2018-05-13 16:22:51 -0500

    added Component interface

---
+++ Component.py
@@ -0,0 +1,23 @@
+""" This a mining rig component Class along with its Interaface Class """
+from abc import ABC, abstractmethod
+
+class IComponent(ABC):
+	@abstractmethod
+	def get_info(self):
+		pass
+	@abstractmethod
+	def get_stats(self):
+		pass
+
+
+class Component(IComponent):
+	m_id = ""      # component name
+	m_info = {}    # dictionary with info
+	m_stats = {}   # dictionary with stats
+	
+	@abstractmethod
+	def get_info(self):
+		pass
+	@abstractmethod
+	def get_stats(self):
+		pass	
